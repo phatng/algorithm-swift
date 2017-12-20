8e27294c83a7f0d2f891c17ff584798e7ebfd2d5
Phat Nguyen <phat.nguyen4@gmail.com>
2017-12-19 16:41:20 -0800

    Adding more descriptive name for generic data element.

--- src/data-structure/Queue.swift
+++ src/data-structure/Queue.swift
@@ -1,22 +1,22 @@
 // Queue: First in, first out
 // O(1) for enqueue and dequeue
 
-public struct Queue<T> {
-  private var queue = [T]()
+public struct Queue<Element> {
+  private var queue = [Element]()
 
   public var count: Int {
     return queue.count
   }
 
-  public var front: T? {
+  public var front: Element? {
     return queue.first
   }
 
-  public mutating func enqueue(_ element: T) {
+  public mutating func enqueue(_ element: Element) {
     queue.append(element)
   }
 
-  public mutating func dequeue() -> T? {
+  public mutating func dequeue() -> Element? {
     return queue.removeFirst()
   }
 }
--- src/data-structure/Stack.swift
+++ src/data-structure/Stack.swift
@@ -1,22 +1,22 @@
 // Stack: Last in, first out
 // O(1) for push and pop
 
-public struct Stack<T> {
-  private var stack = [T]()
+public struct Stack<Element> {
+  private var stack = [Element]()
 
   public var count: Int {
     return stack.count
   }
 
-  public var top: T? {
+  public var top: Element? {
     return stack.last
   }
 
-  public mutating func push(_ element: T) {
+  public mutating func push(_ element: Element) {
     stack.append(element)
   }
 
-  public mutating func pop() -> T? {
+  public mutating func pop() -> Element? {
     return stack.removeLast()
   }
 }
